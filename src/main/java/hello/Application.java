80b65b79d039f3806f0e0be1956e9662cf05b2a6
defindal <defindal@gmail.com>
2017-12-13 09:31:59 +0700

    refactor

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,6 +2,12 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import model.Greeting;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +15,12 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
+    @Bean("abc")
+    public Greeting sample(){
+    	return new Greeting(18, "BPKP");
+    	
+    }
+    
+   
 }
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,13 +1,30 @@
 package hello;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import model.Actor;
+import model.Film;
+import model.Greeting;
 
 @RestController
 public class GreetingController {
@@ -15,10 +32,34 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @Autowired
+    private Greeting x;    
+    
+    @Autowired
+    private  EntityManagerFactory em;
+    
+    @CrossOrigin(origins={"*"})
+    @RequestMapping("/actors")
+    public List<Actor> allActors(){
+    	return	em	.createEntityManager()
+    			.createQuery("from Actor")
+    			.getResultList();   	
+    	
+    }
+    
+    @CrossOrigin(origins={"*"})
+    @RequestMapping("/films")
+    public List<Film> allFilms(){
+    	Query q =	em	.createEntityManager()
+    			.createQuery("from Film");
+    			
+		return q.getResultList();   	
+    	
+    }
+    
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+        return x;
     }
     
     @RequestMapping("/data")
@@ -34,7 +75,38 @@ public class GreetingController {
     		.filter(line -> 
     			line.startsWith(prefix))
     		.collect(Collectors.toList());
-    	
+    	    	
+    }
+    
+    @RequestMapping("/countries")
+    public String getCountries() throws IOException{
+    	URL url = new URL
+    			("http://www.webservicex.net/country.asmx/GetCountries");
+		URLConnection connection =
+				url.openConnection();
+		connection.setDoOutput(true);
+		connection.setRequestProperty(
+				"Content-Type", 
+				"application/x-www-form-urlencoded");
+    	connection.setRequestProperty(
+    			"Content-Length", "0");
+		InputStream stream =
+				connection.getInputStream();		
+		InputStreamReader reader =
+				new InputStreamReader(stream);		
+		BufferedReader buffer =
+				new BufferedReader(reader);
+		
+		String line;
+		StringBuilder builder =
+				new StringBuilder();
+		while((line = buffer.readLine()) != null){
+			builder.append(line);
+		}
+		
+    	return builder.toString();
     	
     }
+    
+    
 }
